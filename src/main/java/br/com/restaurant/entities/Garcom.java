54b6184037581c75f68501f80cda5548e8891307
Rodrigo Hisashi <rodrigohisa@gmail.com>
2021-11-08 13:20:45 -0300

    Garcom adicionado

---
+++ src/main/java/br/com/restaurant/entities/Garcom.java
@@ -0,0 +1,22 @@
+package br.com.restaurant.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Garcom {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String nome;
+
+}
