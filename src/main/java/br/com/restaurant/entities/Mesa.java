b0b383ad2ec7f2e7e9450b2f5191e03db0996a48
DESKTOP-T42G74F\ruibrabo <ruibiecco@gmail.com>
2021-11-08 12:11:41 -0300

    Start project

---
+++ src/main/java/br/com/restaurant/entities/Mesa.java
@@ -0,0 +1,22 @@
+package br.com.restaurant.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Mesa {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private int numeroMesa;
+}
